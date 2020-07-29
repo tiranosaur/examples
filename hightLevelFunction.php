124d59aa30421b10c76e3480c86045c20d0db4f7
tiranosaur <tiranosaur88@gmail.com>
2020-07-29 10:02:07 +0300

    gitignore

--- hightLevelFunction.php
+++ hightLevelFunction.php
@@ -1,6 +1,19 @@
 <?php
 
-$arr = range(1, 10);
-var_dump(array_map(function ($item) {return $item * $item;}, $arr));
-var_dump(array_filter($arr, function ($item){return $item%2==0;}));
-var_dump(array_reduce($arr, function ($item, $result) {return $result + $item;}));
+$arr    = range(1, 10);
+$rndArr = array_map(fn($item) => rand(1, 100), range(1, 10));
+
+var_dump(array_map(function ($item) {
+    return $item * $item;
+}, $arr));
+
+var_dump(array_filter($arr, function ($item) {
+    return $item % 2 == 0;
+}));
+
+var_dump(array_reduce($arr, function ($item, $result) {
+    return $result + $item;
+}));
+
+uasort($rndArr, fn($a, $b) => $a >= $b ? true : false);
+var_dump($rndArr);
--- index.php
+++ index.php
@@ -1,2 +1,18 @@
-<?php
-
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<ul>
+    <ol><a href="/closure.php">Closure</a></ol>
+    <ol><a href="/generator.php">Generator</a></ol>
+    <ol><a href="/hightLevelFunction.php">Hight Level Function</a></ol>    
+    <ol><a href="/solid_O.php">Solid O</a></ol>
+</ul>
+</body>
+</html>
