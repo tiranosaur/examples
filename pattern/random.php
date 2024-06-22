e7decacc77157468e123d6b43b577f97e6758f8d
tiranosaur <tiranosaur88@gmail.com>
2024-06-22 17:09:15 +0300

    remove patterns

--- pattern/random.php
+++
@@ -1,8 +0,0 @@
-<?php
-$arr = [
-    'Singleton - одиночка',
-    'Factory - фабрика',
-    'Absctact Factory - абстрактная фабрика',
-    'Builder - строитель'
-];
-echo $arr[random_int(0, count($arr))];
